c0a89378d63e7480c31f32c3f403cf0006f72c55
SUPERMECHM500 <thedestoryer11@gmail.com>
2020-06-09 21:43:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ with open(join(getcwd(), "Serialized", "bot_config.pkl"), "rb") as f:
             debug_mode = config_data["debug_mode"]
             auto_pull = config_data["auto_pull"]
             tz = config_data["tz"]
-            print(f"Loaded bot_default.pkl{' '*5}")
+            print(f"Loaded bot_default.pkl{' '*20}")
         except KeyError:
             print(f'[Using defaults] bot_config.pkl file improperly formatted.{" "*35}')  # print excess spaces to fully overwrite the '\r' above
             debug_mode = False  # Print exceptions to stdout. Some errors will not be printed for some reason, such as NameError outside of commands.
